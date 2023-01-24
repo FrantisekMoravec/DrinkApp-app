b2580021a2dfe1cb8e0879a8f90fcb2f2778d581
FrantisekMoravec <fmoravec45@gmail.com>
2023-01-24 20:45:31 +0100

    splash screen animace a přechod do home screenu

--- app/src/main/java/com/example/drinkapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/drinkapp/presentation/screens/splash/SplashScreen.kt
@@ -1,14 +1,17 @@
 package com.example.drinkapp.presentation.screens.splash
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -16,18 +19,32 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.example.drinkapp.R
+import com.example.drinkapp.navigation.Screen
 import com.example.drinkapp.ui.theme.DarkBlue
 import com.example.drinkapp.ui.theme.LightBlue
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Splash()
-    //navController.popBackStack()
-    //navController.navigate(Screen.Home.route)
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 3000
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(4000)
+        navController.popBackStack()
+        navController.navigate(Screen.Home.route)
+    }
+    Splash(alpha = alphaAnim.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(alpha: Float) {
     if (isSystemInDarkTheme()){
         Box(
             modifier = Modifier
@@ -58,11 +75,11 @@ fun Splash() {
 @Preview
 @Composable
 fun SplashScreenPreview() {
-    Splash()
+    Splash(alpha = 1f)
 }
 
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun SplashScreenPreviewDark() {
-    Splash()
+    Splash(alpha = 1f)
 }
